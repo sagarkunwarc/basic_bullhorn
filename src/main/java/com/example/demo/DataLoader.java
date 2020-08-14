bcc26b864bd29151f9f4e4639f0512ccc5801784
skunwarcc <sagar.kunwar24@gmail.com>
2020-08-13 21:40:56 -0400

    add service

--- src/main/java/com/example/demo/DataLoader.java
+++ src/main/java/com/example/demo/DataLoader.java
@@ -2,21 +2,23 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
-public class DataLoader implements CommandLineRunner{
-    @Autowired
-    MessageRepository repository;
+@Service
+public class DataLoader implements CommandLineRunner {
 
-    @Override
-    public void run(String... strings) throws  Exception{
-        Message message = new Message("politics","05/20/2019","bush");
-        repository.save(message);
+  @Autowired
+  MessageRepository repository;
 
-        message = new Message("sports","05/22/2019","obama");
-        repository.save(message);
-        message = new Message("economics","05/23/2019","trump");
-        repository.save(message);
-    }
+  @Override
+  public void run(String... strings) throws Exception {
+    Message message = new Message(1L,"politics", "05/20/2019", "bush");
+    repository.save(message);
+
+    message = new Message(2L,"sports", "05/22/2019", "obama");
+    repository.save(message);
+
+    message = new Message(3L, "economics", "05/23/2019", "trump");
+    repository.save(message);
+  }
 }
